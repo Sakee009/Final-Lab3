28d3e21ca80ab2889decba7f61756d7707a1b1ef
Saeed Ibn Sala Sakee <22732592+Sakee009@users.noreply.github.com>
2018-03-20 22:07:26 +0600

    Add files via upload

---
+++ picture.php
@@ -0,0 +1,18 @@
+<?php
+   include"include/header.php";
+
+?>
+<fieldset>
+    <legend><b>PROFILE PICTURE</b></legend>
+    <form>
+        <img width="128" src="../image/user.png" />
+        <br />
+        <input type="file">
+        <hr />
+        <input type="submit" value="Submit">
+    </form>
+</fieldset>
+<?php
+    include"include/footer.php";    
+
+?>
---
+++ public.php
@@ -0,0 +1,15 @@
+ <?php
+   include"include/header.php";
+
+?>
+<tr>
+     <td align="center">
+		<br/>
+			<iframe name="iFrame" frameborder="0" width="65%" height="470" src="read/public_home.html"></iframe>
+		</td>
+		<br>
+<?php
+    include"include/footer.php";    
+
+?>
+   
